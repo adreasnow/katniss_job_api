910ffa4006d78805257dbb57af771047faac3856
Adrea Snow <adrea.snow@gmail.com>
2023-03-21 20:51:04 +1100

    and again...

---
+++ app.py
@@ -0,0 +1,29 @@
+from .db_handler import JobsDB
+from flask import Flask
+import pandas as pd
+
+def main() -> None:
+    app = Flask(__name__)
+    JobsDB(generate=True)
+
+    @app.route('/id/<string:job_ID>/<string:status>/<string:cluster>/', methods=['GET', 'POST'])
+    def id(job_ID: str, status: str, cluster: str) -> str:
+        with JobsDB() as jobsDB:
+            out = jobsDB.add_job(job_ID, status, cluster)
+        return out
+
+    @app.route('/db/', methods=['GET', 'POST'])
+    def db() -> str:
+        with JobsDB() as jobsDB:
+            out = jobsDB.query()
+        return out
+
+    @app.route('/', methods=['GET', 'POST'])
+    def test() -> str:
+        return 'Running!'
+
+    app.run(host='api.adreasnow.com')
+
+if __name__ == '__main__':
+    main()
+    
