cc927eece759bbbb08b18a0ec84095ea08317bdf
Adrea Snow <adrea.snow@gmail.com>
2023-03-21 18:26:07 +1100

    wsgi-ify

--- __init__.py
+++ __init__.py
@@ -3,26 +3,20 @@ from flask import Flask
 import pandas as pd
 import urllib.request
 
-def main() -> None:
+app = Flask(__name__)
+# JobsDB(generate=True)
 
-    app = Flask(__name__)
-    JobsDB(generate=True)
+@app.route('/id/<string:job_ID>/<string:status>/<string:cluster>/', methods=['GET', 'POST'])
+def id(job_ID: str, status: str, cluster: str) -> str:
+    with JobsDB() as jobsDB:
+        out = jobsDB.add_job(job_ID, status, cluster)
+    return out
 
-    @app.route('/id/<string:job_ID>/<string:status>/<string:cluster>/', methods=['GET', 'POST'])
-    def id(job_ID: str, status: str, cluster: str) -> str:
-        with JobsDB() as jobsDB:
-            out = jobsDB.add_job(job_ID, status, cluster)
-        return out
-
-    @app.route('/db/', methods=['GET', 'POST'])
-    def db() -> str:
-        with JobsDB() as jobsDB:
-            out = jobsDB.query()
-        return out
-
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-    app.run(host=external_ip, port=5001, threaded=True)
-
+@app.route('/db/', methods=['GET', 'POST'])
+def db() -> str:
+    with JobsDB() as jobsDB:
+        out = jobsDB.query()
+    return out
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=5151)
